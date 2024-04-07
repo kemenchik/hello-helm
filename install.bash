23981c1488af3f5b8150a18bdf13b5b8e51c61f9
Semyon Klimov <kemenchik.email>
2024-04-07 22:26:25 +0300

    test

--- install.bash
+++ install.bash
@@ -1,5 +1,4 @@
-helm install "hello" ./ --values "values.yaml" -f $BASE_64_KUBECONFIG | base64 --decode \
-  --set configmap.hello_message=$OVERRIDE_MESSAGE \
-  --set hello.image=$HELLO_IMAGE \
-	--set teamcity.agent.server_url=$TEAMCITY_SERVER_URL \
-	--set ingress.host=$INGRESS_HOST
+echo -n "$BASE_64_KUBECONFIG" | base64 --decode > ./conf
+KUBECONFIG_PATH=$(pwd)/conf
+
+helm install "hello" ./ --values "values.yaml" --force -f $KUBECONFIG_PATH --set configmap.hello_message=$OVERRIDE_MESSAGE --set hello.image=$HELLO_IMAGE --set teamcity.agent.server_url=$TEAMCITY_SERVER_URL --set ingress.host=$INGRESS_HOST
