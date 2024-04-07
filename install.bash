4a31ccf2528437e4dff05faefcde69d146e5a0a5
Semyon Klimov <kemenchik.email>
2024-04-07 21:32:08 +0300

    Initial commit

---
+++ install.bash
@@ -0,0 +1,5 @@
+helm install "hello" ./ --values "values.yaml" -f $BASE_64_KUBECONFIG | base64 --decode \
+  --set configmap.hello_message=$OVERRIDE_MESSAGE \
+  --set hello.image=$HELLO_IMAGE \
+	--set teamcity.agent.server_url=$TEAMCITY_SERVER_URL \
+	--set ingress.host=$INGRESS_HOST
